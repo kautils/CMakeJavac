42df7731eafc79354c581d78d094549181e8dc35
ka <todo1@rotarydef.net>
2023-09-19 04:39:53 -0700

    first

--- CMakeJavac.cmake
+++ CMakeJavac.cmake
@@ -3,39 +3,40 @@ macro(CMakeJavac)
     set(${PROJECT_NAME}_m_evacu ${m})
     set(m ${PROJECT_NAME}.CMakeJavac)
     list(APPEND ${m}_unsetter 
-            ${m}_VERBOSE ${m}_BOOTCLASSPATH ${m}_JAVAC 
+            ${m}_DEBUG_VERBOSE ${m}_VERBOSE ${m}_BOOTCLASSPATH ${m}_JAVAC 
             ${m}_SOURCE ${m}_TARGET ${m}_DESTINATION ${m}_CLASSPATH 
-            ${m}_SOURCEPATH ${m}_FILES ${m}_WORKING_DIRECTORY)
+            ${m}_SOURCEPATH ${m}_FILES ${m}_WORKING_DIRECTORY )
     
     cmake_parse_arguments(${m}
-        "VERBOSE" "BOOTCLASSPATH;JAVAC;SOURCE;TARGET;DESTINATION;WORKING_DIRECTORY" "CLASSPATH;SOURCEPATH;FILES" 
+        "VERBOSE;DEBUG_VERBOSE" "BOOTCLASSPATH;JAVAC;SOURCE;TARGET;DESTINATION;WORKING_DIRECTORY" "CLASSPATH;SOURCEPATH;FILES" 
         ${ARGV}
     )
     
     if(NOT DEFINED ${m}_WORKING_DIRECTORY)
         set(${m}_WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
     endif()
-        
-    include(CMakePrintHelpers)
-    foreach(__var ${${m}_unsetter})
-        cmake_print_variables(${__var})
-    endforeach()
     
-    if(${is_verbose})
-        message("\"${${m}_JAVAC}\" -source ${${m}_source} -target ${${m}_target} -bootclasspath \"${${m}_bootclasspath}\" -cp \"${${m}_cp}\" -sourcepath \"${${m}_sourcepath}\" ${${m}_javas} ${${m}_gen} -d ${${m}_dest}")
+    if(${${m}_DEBUG_VERBOSE})
+        include(CMakePrintHelpers)
+        foreach(__var ${${m}_unsetter})
+            cmake_print_variables(${__var})
+        endforeach()
     endif()
     
-    list(APPEND ${m}_unsetter ${m}_delm)
+    list(APPEND ${m}_unsetter ${m}_echo ${m}_sourcepath"${${m}_delm}" ${m}_sourcepath ${m}_res ${m}_out ${m}_err)
+    if(${${m}_VERBOSE})
+        set(${m}_echo COMMAND_ECHO)
+    endif()
+    
     if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
         set(${m}_delm ";")
     else()
         set(${m}_delm ":")
     endif()
     
-    list(APPEND ${m}_unsetter  ${m}_sourcepath"${${m}_delm}" ${m}_sourcepath ${m}_res ${m}_out ${m}_err)
-    
     list(JOIN ${m}_CLASSPATH "${${m}_delm}" ${m}_CLASSPATH)
     list(JOIN ${m}_SOURCEPATH "${${m}_delm}" ${m}_SOURCEPATH)
+    
     execute_process(
         COMMAND "${${m}_JAVAC}"
         -source ${${m}_SOURCE} 
@@ -45,6 +46,7 @@ macro(CMakeJavac)
         -sourcepath "${${m}_SOURCEPATH}"
         ${${m}_FILES}
         -d ${${m}_DESTINATION}
+        ${${m}_echo}
         RESULT_VARIABLE ${m}_res
         OUTPUT_VARIABLE ${m}_out
         ERROR_VARIABLE ${m}_err
